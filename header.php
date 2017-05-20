e491b22f23d4499038dd52fb7a2bdc3caec21a51
bankie606 <frcrescioni@gmail.com>
2017-05-20 12:27:39 -0700

    06) Create Your Main & Sub Menu Systems
    Finally finally finally fixed the menu system up.

--- header.php
+++ header.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<!-- saved from url=(0046)http://ridethegrid.net/web200/final/events.php -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Fred Wildlife Refuge</title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
-        <link rel="stylesheet" href="css/lightbox.css" />
-        <link rel="stylesheet" href="css/normalize.css">
-        <script src="js/buttonbackgroundchange.js"></script>
     </head>
     <body data-feedly-mini="yes">
         <div id="mainwrapper"> 
@@ -16,69 +13,32 @@
     
             
             <h1 id="fredtext">FRED WILDLIFE REFUGE</h1>
-<!-- Begin Navigation -->
+	<!-- Begin Navigation -->
 
 
 
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'nav','container_id' => 'navigation', 'items_wrap' => '<ul id="navigation" class="%2$s">%3$s</ul>', ) ); ?>
+<?php wp_nav_menu( 
+	array( 
+	'menu' => '', 
+	'container' => 'ul', 
+	'container_class' => '', 
+	'menu_id' => 'navigation', 
+ 	'items_wrap' => '<ul></ul>', 
+	'item_spacing' => 'preserve',
+    	'theme_location' => 'main-menu', 
+	) 
+); 
+?>
  
 
-            
+
+
           
-          <!-- End Navigation -->
-        
- 
-            <!--begin navigation -->
-      
-            
-<!--
-  <nav class="menu">
-      <ul id="navigation-items">
-            
- <li><a href="#">ABOUT FRED</a></li>
- <li class="active"><a href="#">EVENTS</a>
-  <ul>
-   <li><a href="#">Past</a></li>
-   <li><a href="#">Present</a></li>
-   <li><a href="#">Future</a></li>
-  </ul>
- </li>
- <li><a href="#">VIDEOWALL</a>
- </li>
- <li><a href="#">PARTNERS</a>
-                  <ul>
-            <li><a href="#">Past Partners</a></li>
-   <li><a href="#">Become a Partner</a></li>
-            </ul>
-            </li>
--->
-<!--
- <li><a href="#">CONTACT</a></li>
-    <li><a href="https://www.facebook.com/fredwildliferefuge/?fref=ts" rel="nofollow" target="_blank"><span class="menuicon"><img src="<?php bloginfo('template_directory'); ?>/images/facebookicon.png" id="facebookicon" alt="facebook icon"></span></a></li>
-    <li><a href="https://twitter.com/fredwildlife" rel="nofollow" target="_blank"><span class="menuicon"><img src="<?php echo get_bloginfo('template_url') ?>/images/twittericon.png" id="twittericon" alt="twitter icon"></span></a></li>
-</ul>            
-        </nav>
-            
-             Begin Mobile Navigation 
-<nav id="mobileNav">
-    <ul>
-    <li><a href="#">ABOUT FRED</a></li>
-    <li><a href="#">EVENTS</a>
-    </li><li><a href="#">VIDEOWALL</a>
- </li>
-    <li><a href="#">PARTNERS</a></li>    
-    <li><a href="#">CONTACT</a></li>
-    </ul>
-</nav>
--->
-<!-- End Mobile Navigation -->
-            
-            
-            
-            <!--end navigation -->
-<video autoplay muted poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" class="backgroundvid" loop>
+        <!--End Navigation -->
+<video autoplay muted poster="http://web170.frcrescioni.net/wordpress/wp-content/uploads/2017/04/chillfredlogo.jpg" id="bgvid" class="backgroundvid" loop>
   <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
 <source id='motionbackground' src="http://ridethegrid.net/web200/final/media/guitartime.mp4" type="video/mp4">
 </video>
 
             <!--begin content -->
+            
